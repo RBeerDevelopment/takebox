afea8456a2a1b09839ae6517d423de82fd71442b
Robin Beer <contact@robin-beer.com>
2023-04-23 08:43:17 +0200

    add sign in with apple to app

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -1,23 +1,44 @@
 import { useOAuth } from "@clerk/clerk-expo";
 import React from "react";
-import { Button, View } from "react-native";
+import { View, Text } from "react-native";
 import { useWarmUpBrowser } from "../hooks/useWarmUpBrowser";
+import { StyledButton } from "./button";
 
 const SignInWithOAuth = () => {
   useWarmUpBrowser();
 
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_discord" });
+  const discordSignIn = useOAuth({ strategy: "oauth_discord" });
+  const appleSignIn = useOAuth({ strategy: "oauth_apple" });
 
   const handleSignInWithDiscordPress = React.useCallback(async () => {
     try {
-      const { createdSessionId, signIn, signUp, setActive } =
-        await startOAuthFlow();
+      const { createdSessionId, setActive } =
+        await discordSignIn.startOAuthFlow();
       if (createdSessionId) {
         setActive({ session: createdSessionId });
       } else {
         // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
-        throw new Error("There are unmet requirements, modifiy this else to handle them")
+        throw new Error(
+          "There are unmet requirements, modifiy this else to handle them",
+        );
+      }
+    } catch (err) {
+      console.log(JSON.stringify(err, null, 2));
+      console.log("error signing in", err);
+    }
+  }, []);
 
+  const handleSignInWithApple = React.useCallback(async () => {
+    try {
+      const { createdSessionId, setActive } =
+        await appleSignIn.startOAuthFlow();
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Modify this code to use signIn or signUp to set this missing requirements you set in your dashboard.
+        throw new Error(
+          "There are unmet requirements, modifiy this else to handle them",
+        );
       }
     } catch (err) {
       console.log(JSON.stringify(err, null, 2));
@@ -26,13 +47,15 @@ const SignInWithOAuth = () => {
   }, []);
 
   return (
-    <View className="rounded-lg border-2 border-gray-500 p-4">
-      <Button
-        title="Sign in with Discord"
+    <View className="flex flex-col">
+      <Text>Start using TakeBox </Text>
+      <StyledButton
+        text="Sign In with Discord"
         onPress={handleSignInWithDiscordPress}
       />
+      <StyledButton text="Sign In with Apple" onPress={handleSignInWithApple} />
     </View>
   );
-}
+};
 
 export default SignInWithOAuth;
---
+++ apps/expo/src/components/button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+
+interface Props {
+  text: string;
+  onPress: () => void;
+}
+
+export function StyledButton(props: Props): React.ReactElement {
+  const { text, onPress } = props;
+  return (
+    <TouchableOpacity
+      className="my-2 flex w-full items-center justify-center rounded-lg bg-white py-2"
+      onPress={onPress}
+    >
+      <Text className="text-lg text-black ">{text}</Text>
+    </TouchableOpacity>
+  );
+}
