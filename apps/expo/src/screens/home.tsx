6d386974870517a5ffd743572d3803733d62ab9e
Robin Beer <contact@robin-beer.com>
2023-04-23 10:02:16 +0200

    add default layout

--- apps/expo/src/screens/home.tsx
+++ apps/expo/src/screens/home.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
 import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Layout } from "../components/layout";
 
-export const HomeScreen = () => {
+export function HomeScreen() {
   return (
-    <SafeAreaView className="bg-[#2e026d] bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <Layout>
       <View className="h-full w-full items-center justify-center">
         <Text className="text-lg italic text-white">
           Nothing to see here yet...
         </Text>
       </View>
-    </SafeAreaView>
+    </Layout>
   );
-};
+}
