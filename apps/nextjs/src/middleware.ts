682b10f39f7432e03234ca3e66192a2d130d020e
Robin Beer <contact@robin-beer.com>
2023-07-12 14:04:29 +0200

    convert middleware back to old version

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,7 +1,21 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse, type NextRequest } from "next/server";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware();
+export default withClerkMiddleware((_req: NextRequest) => {
+  return NextResponse.next();
+});
 
+// Stop Middleware running on static files
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Match request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     *
+     * This includes images, and requests from TRPC.
+     */
+    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+  ],
 };
