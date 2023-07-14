12d045d36828f3062e18ed7f72f55d12efefe089
Robin Beer <contact@robin-beer.com>
2023-07-14 19:39:46 +0200

    extract clerk key for expo to env, update @clerk/expo, rename app to Flavoury

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -1,51 +1,50 @@
-const CLERK_PUBLISHABLE_KEY =
-    "pk_test_c3VidGxlLWZseS05OC5jbGVyay5hY2NvdW50cy5kZXYk";
+const CLERK_PUBLISHABLE_KEY = process.env.CLERK_PUBLISHABLE_KEY;
 
 process.env.EXPO_ROUTER_APP_ROOT = __dirname + "/src/app";
 
 const defineConfig = () => ({
-    name: "TakeBox",
-    slug: "TakeBox",
-    version: "1.0.0",
-    scheme: "beer.robin.takebox",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
-    userInterfaceStyle: "automatic",
-    splash: {
-        image: "./assets/icon.png",
-        resizeMode: "contain",
-        backgroundColor: "#ffffff",
+  name: "Flavoury",
+  slug: "Flavoury",
+  version: "1.0.0",
+  scheme: "beer.robin.takebox",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: false,
+    bundleIdentifier: "beer.robin.takebox",
+    buildNumber: "3",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#2e026d",
     },
-    updates: {
-        fallbackToCacheTimeout: 0,
+  },
+  extra: {
+    eas: {
+      projectId: "50e70c62-232a-40f3-8d5f-9c92eae92580",
     },
-    assetBundlePatterns: ["**/*"],
-    ios: {
-        supportsTablet: false,
-        bundleIdentifier: "beer.robin.takebox",
-        buildNumber: "2",
-    },
-    android: {
-        adaptiveIcon: {
-            foregroundImage: "./assets/icon.png",
-            backgroundColor: "#2e026d",
-        },
-    },
-    extra: {
-        eas: {
-            projectId: "50e70c62-232a-40f3-8d5f-9c92eae92580",
-        },
-        CLERK_PUBLISHABLE_KEY,
-    },
-    plugins: [
-        "./expo-plugins/with-modify-gradle.js",
-        [
-            "expo-location",
-            {
-                locationAlwaysAndWhenInUsePermission: `Allow TakeBox to use your location, to show you restaurants near you.`,
-            },
-        ],
+    CLERK_PUBLISHABLE_KEY,
+  },
+  plugins: [
+    "./expo-plugins/with-modify-gradle.js",
+    [
+      "expo-location",
+      {
+        locationAlwaysAndWhenInUsePermission: `Allow TakeBox to use your location, to show you restaurants near you.`,
+      },
     ],
+  ],
 });
 
 export default defineConfig;
