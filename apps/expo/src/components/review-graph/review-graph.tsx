266a464b412cbe6f4de83d0542e00e5548c6c308
Robin Beer <contact@robin-beer.com>
2023-07-28 18:02:03 +0200

    add dark theme for graph

--- apps/expo/src/components/review-graph/review-graph.tsx
+++ apps/expo/src/components/review-graph/review-graph.tsx
@@ -13,14 +13,14 @@ export function ReviewGraph(props: Props): React.ReactElement {
   const maxCount = Math.max(...Object.values(reviewCounts));
 
   return (
-    <View className="flex h-20 flex-row items-end justify-center gap-2 px-2">
+    <View className="flex h-20 flex-row items-end justify-center gap-1">
       {Object.values(reviewCounts).map((count, idx) => {
         const height = (count / maxCount) * 95 + 1;
         return (
           <View
             key={idx}
             style={{ height: `${height}%` }}
-            className={`w-[calc(6%)] bg-primary`}
+            className={`dark:bg-primary-dark w-[calc(7%)] bg-primary`}
           />
         );
       })}
