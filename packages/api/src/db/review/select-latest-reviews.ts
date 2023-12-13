ed9bdcaf0b69792f0a729459979089b41a21618c
Robin Beer <contact@robin-beer.com>
2023-12-13 09:04:00 +0100

    update latest reviews to only be shown in 5km radius

--- packages/api/src/db/review/select-latest-reviews.ts
+++ packages/api/src/db/review/select-latest-reviews.ts
@@ -1,11 +1,16 @@
 import type { PrismaClient } from "@flavoury/db";
 
+import { generateBoundingBoxAsCoordinates } from "../../helper/generate-bounding-box-as-coordinates";
 import type { ReviewListItem } from "./review-list-item";
 
 export async function selectLastestReviews(
   prisma: PrismaClient,
+  lat: number,
+  lng: number,
   take: number,
 ): Promise<ReviewListItem[]> {
+  const boundingBox = generateBoundingBoxAsCoordinates({ lat, lng }, 5_000);
+
   const reviews = await prisma.review.findMany({
     select: {
       id: true,
@@ -23,6 +28,18 @@ export async function selectLastestReviews(
         },
       },
       content: true,
+    },
+    where: {
+      restaurant: {
+        lat: {
+          gte: boundingBox.min.lat,
+          lte: boundingBox.max.lat,
+        },
+        lng: {
+          gte: boundingBox.min.lng,
+          lte: boundingBox.max.lng,
+        },
+      },
     },
     orderBy: [{ date: "desc" }, { updatedAt: "desc" }],
     take,
