5bf2d5a7ea2053b5b7d32357497b42ae569f9f43
Robin Beer <contact@robin-beer.com>
2023-12-13 09:51:31 +0100

    add type fix for apple button

--- apps/expo/src/components/sign-in-with-apple-button.tsx
+++ apps/expo/src/components/sign-in-with-apple-button.tsx
@@ -13,7 +13,7 @@ export function SignInWithAppleButton(props: Props): React.ReactElement {
     <TouchableOpacity className="flex h-14 w-full" onPress={onPress}>
       <View className="flex h-fit flex-row items-center justify-center rounded-full bg-white px-4 py-3">
         <View className="absolute left-6">
-          <Icon size={28} name={"apple" as any} />
+          <Icon size={28} name={"apple" as unknown as "key"} />
         </View>
         <Text className="text-lg font-bold">Sign in with Apple</Text>
       </View>
