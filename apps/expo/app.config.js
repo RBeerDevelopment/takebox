7e639646d2e13d94714ba74fd12820e3125ee486
Robin Beer <contact@robin-beer.com>
2023-12-27 11:40:50 +0100

    add non-exempt encryption config, bump version

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -7,7 +7,7 @@ process.env.EXPO_ROUTER_APP_ROOT = __dirname + "/src/app";
 const defineConfig = () => ({
   name: "Flavoury",
   slug: "TakeBox",
-  version: "0.11.0",
+  version: "0.11.1",
   scheme: "beer.robin.takebox",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -24,8 +24,11 @@ const defineConfig = () => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: "beer.robin.takebox",
-    buildNumber: "23",
+    buildNumber: "24",
     associatedDomains: ["applinks:flavoury.app"],
+    config: {
+      usesNonExemptEncryption: false,
+    },
   },
   android: {
     adaptiveIcon: {
