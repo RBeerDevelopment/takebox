c01cfd3623976e3c91f212103a3ccef31a70cb48
Robin Beer <contact@robin-beer.com>
2023-12-31 17:21:22 +0100

    cleanup unused import

--- apps/expo/index.tsx
+++ apps/expo/index.tsx
@@ -1,8 +1,6 @@
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
 
-import "react-native-console-time-polyfill";
-
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context("./src/app");
