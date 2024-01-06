904980905ce7235d7616ebb82f9c1dfe25d0b32b
Robin Beer <contact@robin-beer.com>
2024-01-06 10:57:01 +0100

    bump version

--- apps/expo/app.config.js
+++ apps/expo/app.config.js
@@ -7,7 +7,7 @@ process.env.EXPO_ROUTER_APP_ROOT = __dirname + "/src/app";
 const defineConfig = () => ({
   name: "Flavoury",
   slug: "TakeBox",
-  version: "0.13.0",
+  version: "0.14.0",
   scheme: "beer.robin.takebox",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -24,7 +24,7 @@ const defineConfig = () => ({
   ios: {
     supportsTablet: false,
     bundleIdentifier: "beer.robin.takebox",
-    buildNumber: "25",
+    buildNumber: "26",
     associatedDomains: ["applinks:flavoury.app"],
     config: {
       usesNonExemptEncryption: false,
