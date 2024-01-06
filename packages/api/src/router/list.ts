310671bac50dff4edf6deaedb057923a8bba12eb
Robin Beer <contact@robin-beer.com>
2024-01-06 14:12:55 +0100

    add api functions to update and delete list

--- packages/api/src/router/list.ts
+++ packages/api/src/router/list.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 import { createList } from "../functions/list/create-list";
+import { deleteList } from "../functions/list/delete-list";
 import { getListDetailsById } from "../functions/list/get-list-details-by-id";
 import { getOwnLists } from "../functions/list/get-own-lists";
 import { toggleRestaurantInList } from "../functions/list/toggle-restaurant-in-list";
+import { updateList } from "../functions/list/update-list";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const listRouter = createTRPCRouter({
@@ -23,6 +25,30 @@ export const listRouter = createTRPCRouter({
       );
 
       return newList.id;
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        newName: z.string().optional(),
+        newDescription: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { auth, prisma } = ctx;
+      return await updateList(
+        input.id,
+        auth.userId,
+        input.newName,
+        input.newDescription,
+        prisma,
+      );
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const { auth, prisma } = ctx;
+      return await deleteList(input.id, auth.userId, prisma);
     }),
   getOwn: protectedProcedure.query(async ({ ctx }) => {
     const { auth, prisma } = ctx;
